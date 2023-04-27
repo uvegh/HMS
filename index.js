bf0863c60331bf482d6b1178a48983d8f7c0b83a
Godwin <boiikechukwu.godwin@gmail.com>
2023-04-27 19:49:27 -0400

    First commit

---
+++ index.js
@@ -0,0 +1,24 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const mongoose = require( 'mongoose' )
+// Port in Use
+const PORT = 3001
+
+// Setting up the Mongodb connection
+const DBConnection = require( './config/DBConnection' )
+
+// calling the DATABASE
+DBConnection()
+
+// built in middleware to handle urlencoded form-dara
+app.use(express.urlencoded({ extended: true }))
+
+// built in middleware for json
+app.use( express.json() )
+
+// console.log(process.env.DATABASE_URI)
+mongoose.connection.once('open', function () {
+  console.log('connected to mongodb')
+  app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+})
